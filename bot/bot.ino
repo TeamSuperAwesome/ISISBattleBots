8372f53445c90e74edb252b0adec70dba5e04a44
Dan Nixon <dan@dan-nixon.com>
2015-06-16 12:51:27 +0100

    Control magic

---
+++ bot/bot.ino
@@ -0,0 +1,73 @@
+#include <Servo.h>
+
+#define SERIAL_DEBUG
+
+#define SERVO_CENTRE 1500
+
+#define LEFT_SERVO_PIN 9
+#define RIGHT_SERVO_PIN 10
+
+
+Servo g_servoLeft;
+Servo g_servoRight;
+
+
+void setup()
+{
+#ifdef SERIAL_DEBUG
+  Serial.begin(9600);
+#endif
+
+  g_servoLeft.attach(LEFT_SERVO_PIN);
+  g_servoRight.attach(RIGHT_SERVO_PIN);
+}
+
+
+void loop()
+{
+  processMotion(500, 0);
+  delay(1000);
+  processMotion(-500, 0);
+  delay(1000);
+  processMotion(0, -500);
+  delay(1000);
+  processMotion(0, 500);
+  delay(1000);
+}
+
+
+/**
+ * Do motion related stuffs.
+ *
+ * I'm lazy so it can only do either rotational or linear motion, ze.
+ *
+ * @param linearVel Velocity in the linear direction (positive is forward)
+ * @param angularVel Velocity in the angular direction (positive is right)
+ * @return Boolean indicating if you read the f***ing manual
+ */
+bool processMotion(int16_t linearVel, int16_t angularVel)
+{
+  if(linearVel != 0 && angularVel != 0)
+    return false;
+
+  int16_t left = SERVO_CENTRE;
+  int16_t right = SERVO_CENTRE;
+
+  left += linearVel;
+  right -= linearVel;
+
+  left += angularVel;
+  right += angularVel;
+
+  g_servoLeft.writeMicroseconds(left);
+  g_servoRight.writeMicroseconds(right);
+
+#ifdef SERIAL_DEBUG
+  Serial.print("Left servo (ns): ");
+  Serial.println(left);
+  Serial.print("Right servo (ns): ");
+  Serial.println(right);
+#endif
+
+  return true;
+}
